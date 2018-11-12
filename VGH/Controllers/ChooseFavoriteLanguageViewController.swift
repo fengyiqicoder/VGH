0c363361bbcfbea7a4fa9d6566934b572de90ac1
FinchFeng <329650437@qq.com>
2018-11-12 20:31:36 +0800

    Github Trending 将近完工

---
+++ VGH/Controllers/ChooseFavoriteLanguageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChooseFavoriteLanguageViewController.swift
+//  VGH
+//
+//  Created by 冯奕琦 on 2018/11/12.
+//  Copyright © 2018 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class ChooseFavoriteLanguageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
